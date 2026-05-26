d87a277e4759ee635749a98c1f4e3a64ccd00169
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-26 17:10:41 +0300

    feat: create login and register method

--- server/apps/accounts/views.py
+++ server/apps/accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer,LoginSerializer,UserSerializer
 
-# Create your views here.
+
+
+class AuthViewSet(viewsets.ViewSet):
+
+    @action(detail=False,methods=["post"])
+    def register(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user_data = UserSerializer(user).data
+
+            return Response({
+                "message":"Account successfully created.",
+                "user":user_data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            tokens = serializer.validated_data
+            # user = UserSerializer(serializer.user).data
+
+            return Response({
+                "message": "Login successful",
+                # "user": user,
+                "access": tokens['access'],
+                "refresh": tokens['refresh']
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
