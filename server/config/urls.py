c8052bd432f7c4ca3bfc5018537c49b3f1a7b0bc
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-30 10:12:09 +0300

    feat: add register

--- server/config/urls.py
+++ server/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.accounts.urls')),
+    path('auth/', include('apps.accounts.urls')),
 ]
