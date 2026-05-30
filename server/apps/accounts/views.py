6f887aa69c73267e6fd209b2afa74bd8036b7b0d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-30 11:19:17 +0300

    feat(auth): add register and login endpoints with JWT and custom error handling

--- server/apps/accounts/views.py
+++ server/apps/accounts/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 from .utils import set_auth_cookies
 
 
@@ -27,3 +29,35 @@ class RegisterView(APIView):
         set_auth_cookies(response, refresh)
 
         return response
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(
+            request,
+            email=serializer.validated_data.get("email"),
+            password=serializer.validated_data.get("password")
+        )
+
+
+        if not user:
+            raise AuthenticationFailed("Invalid email or password.")
+
+        refresh = RefreshToken.for_user(user)
+
+        response = Response({
+            'success': True,
+            'message': 'Login successful',
+            'user':    UserSerializer(user).data,
+            'access':  str(refresh.access_token),
+        })
+
+        set_auth_cookies(response, refresh)
+
+        return response
+
