0c94c7fa8f1123344ad6533e64c4d4a0b4e9f71d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-30 10:12:17 +0300

    feat: add register

--- server/apps/accounts/views.py
+++ server/apps/accounts/views.py
@@ -1,51 +1,29 @@
-from rest_framework.decorators import action
-from rest_framework import viewsets,status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import RegisterSerializer,LoginSerializer,UserSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer
+from .utils import set_auth_cookies
 
 
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
 
-class AuthViewSet(viewsets.ViewSet):
-
-    @action(detail=False,methods=["post"])
-    def register(self,request):
+    def post(self, request):
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            user_data = UserSerializer(user).data
+        serializer.is_valid(raise_exception=True)
 
-            return Response({
-                "message":"Account successfully created.",
-                "user":user_data
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
 
-    @action(detail=False, methods=['post'])
-    def login(self, request):
-        serializer = LoginSerializer(data=request.data)
+        response = Response({
+            'success': True,
+            'message': 'Account created successfully.',
+            'accessToken': str(refresh.access_token),
+            'refreshToken': str(refresh)
+        }, status=status.HTTP_201_CREATED)
 
-        if serializer.is_valid():
-            tokens = serializer.validated_data
-            refresh_token = tokens['refresh']
-            access_token = tokens['access']
+        set_auth_cookies(response, refresh)
 
-            response = Response({
-                "message": "Login successful",
-                "access": access_token,
-            })
-
-
-            response['X-Refresh-Token'] = refresh_token
-
-            response.set_cookie(
-                key='refresh_token',
-                value=refresh_token,
-                httponly=True,
-                secure=True,
-                samesite='Lax',
-                max_age=60 * 60 * 24 * 7,
-            )
-
-            return response
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return response
