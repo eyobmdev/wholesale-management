3b975806d79c4c815f4e9586e6a2ca5edb2a1c42
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-31 23:47:49 +0300

    feat(auth): add forget and reset password endpoints

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView,ForgotPasswordView,ResetPasswordView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/',LoginView.as_view(),name='auth_login'),
+    path('forgot-password/', ForgotPasswordView.as_view()),
+    path('reset-password/',  ResetPasswordView.as_view()),
 ]
