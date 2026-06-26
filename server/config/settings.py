f878cd7f3295f4e5f3c4ccacfadaf9eec1bb2423
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-26 13:31:07 +0300

    refactor: split installed app config for convenient

--- server/config/settings.py
+++ server/config/settings.py
@@ -31,22 +31,29 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
-
-    "apps.accounts",
-    "apps.customers",
-    "apps.products"
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    # "corsheaders",
+    # "django_filters",
+    # "drf_spectacular",
+]
+
+LOCAL_APPS = [
+    "apps.accounts",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
