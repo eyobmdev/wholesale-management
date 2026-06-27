fa4c217a9957679319170f060969ca4cd7130630
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-27 21:36:50 +0300

    feat(): add django-filter dependency.

--- server/config/settings.py
+++ server/config/settings.py
@@ -45,7 +45,7 @@ THIRD_PARTY_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     # "corsheaders",
-    # "django_filters",
+    "django_filters",
     # "drf_spectacular",
 ]
 
