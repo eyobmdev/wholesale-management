a2f72a18d79fa0ba4cbff21e40234cd346d1280a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-04 04:37:49 +0300

    improvement: add filter_backends = [DjangoFilterBackend] to views

--- server/apps/payments/views.py
+++ server/apps/payments/views.py
@@ -8,6 +8,7 @@ sale/purchase. Manual records can be freely edited and deleted.
 
 GeneralExpense records are always manually created and can be fully managed.
 """
+from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 
@@ -67,6 +68,7 @@ class CustomerIncomeViewSet(viewsets.ModelViewSet):
     queryset = CustomerIncome.objects.all()
     pagination_class = StandardPagination
     filterset_class = CustomerIncomeFilter
+    filter_backends = [DjangoFilterBackend]
     search_fields = ['customer__name', 'receipt_number', 'notes', 'reference']
     ordering_fields = ['date', 'paid_amount', 'created_at']
     ordering = ['-date', '-created_at']
@@ -137,6 +139,7 @@ class FactoryPaymentViewSet(viewsets.ModelViewSet):
     queryset = FactoryPayment.objects.all()
     pagination_class = StandardPagination
     filterset_class = FactoryPaymentFilter
+    filter_backends = [DjangoFilterBackend]
     search_fields = ['factory__name', 'payment_number', 'notes', 'reference']
     ordering_fields = ['date', 'paid_amount', 'created_at']
     ordering = ['-date', '-created_at']
@@ -201,6 +204,7 @@ class GeneralExpenseViewSet(viewsets.ModelViewSet):
     queryset = GeneralExpense.objects.all()
     pagination_class = StandardPagination
     filterset_class = GeneralExpenseFilter
+    filter_backends = [DjangoFilterBackend]
     search_fields = ['description', 'notes', 'expense_number', 'reference']
     ordering_fields = ['date', 'amount', 'created_at']
     ordering = ['-date', '-created_at']
