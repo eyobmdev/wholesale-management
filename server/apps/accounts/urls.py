2088685ff9376eba0dbd383352952de631355b9f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-09 07:34:19 +0300

    feat: add logout endpoint and import LogoutView in accounts urls

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import RegisterView, LoginView,ForgotPasswordView,ResetPasswordView,ChangePasswordView
+from .views import (RegisterView,
+                    LoginView,
+                    ForgotPasswordView,
+                    ResetPasswordView,
+                    ChangePasswordView,
+                    LogoutView)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/',LoginView.as_view(),name='auth_login'),
     path('forgot-password/', ForgotPasswordView.as_view()),
     path('reset-password/',  ResetPasswordView.as_view()),
-    path('change-password/', ChangePasswordView.as_view())
+    path('change-password/', ChangePasswordView.as_view()),
+    path('logout/', LogoutView.as_view())
 ]
