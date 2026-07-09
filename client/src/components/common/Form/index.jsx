8b57addb2e3a9483d19d227c57d235b0c4cd12de
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-09 21:47:01 +0300

    feat: implement reusable form components including Input, Select, TextArea, Checkbox, and FormField wrapper

---
+++ client/src/components/common/Form/index.jsx
@@ -0,0 +1,5 @@
+export { FormField } from './FormField.jsx';
+export { Input } from './Input.jsx';
+export { Select } from './Select.jsx';
+export { TextArea } from './TextArea.jsx';
+export { Checkbox } from './Checkbox.jsx';
