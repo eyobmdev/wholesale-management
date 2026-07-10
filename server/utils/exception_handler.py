6717c8829887c99a505ee06b710c16a24ffd82c0
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-10 22:59:19 +0300

    feat: add purchase validation rules and refine exception handler message formatting

--- server/utils/exception_handler.py
+++ server/utils/exception_handler.py
@@ -2,8 +2,6 @@ from rest_framework.views import exception_handler
 from rest_framework.exceptions import ValidationError, ParseError
 from rest_framework.response import Response
 from rest_framework import status
-from django.db import IntegrityError
-
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
