5dc8b0fb002abe0cee14ba1bba93945bd6f12b09
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-17 19:21:32 +0300

    feat: implement income edit modal and view details page for enhanced payment management

--- server/apps/factories/urls.py
+++ server/apps/factories/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FactoryViewSet, FactoryOptionViewSet
 
+
 router = DefaultRouter()
 router.register(r'factories', FactoryViewSet, basename='factory')
 router.register(r'factory-options', FactoryOptionViewSet, basename='factory-options')
