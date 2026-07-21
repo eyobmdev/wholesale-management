2a27a20634ab0b28d490a2a821c76ea9e9003dbf
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-21 23:14:11 +0300

    chore: comment out search bar and notification button in header

--- client/src/components/layout/Header/index.jsx
+++ client/src/components/layout/Header/index.jsx
@@ -24,20 +24,20 @@ export default function Header({ onMenuClick }) {
         <h1 className="page-title">Dashboard</h1>
       </div>
       <div className="header-right">
-        <div className="search-bar">
+        {/* <div className="search-bar">
           <i className="ri-search-line"></i>
           <input type="text" placeholder="Search..." />
-        </div>
-        
-        <button 
-          className="icon-btn theme-toggle" 
+        </div> */}
+
+        <button
+          className="icon-btn theme-toggle"
           onClick={() => setIsDark(!isDark)}
           title="Toggle Theme"
         >
           {isDark ? <i className="ri-sun-line"></i> : <i className="ri-moon-line"></i>}
         </button>
-        
-        <button className="icon-btn"><i className="ri-notification-3-line"></i></button>
+
+        {/* <button className="icon-btn"><i className="ri-notification-3-line"></i></button> */}
         <div className="user-profile">
           <div className="avatar"><i className="ri-user-smile-line"></i></div>
           <span className="username">Admin</span>
